98877b37a45dc0f369a41d1f3e98615dd97bcfd7
SandeisonFernandes <97060385+sandeisonfernandes@users.noreply.github.com>
2022-03-28 14:34:56 -0300

    Solução do Botão 4

--- app/src/main/java/com/example/cinapp/MyLocationActivity.java
+++ app/src/main/java/com/example/cinapp/MyLocationActivity.java
@@ -1,29 +1,58 @@
 package com.example.cinapp;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class MyLocationActivity extends Activity implements LocationListener {
-    @SuppressLint("MissingPermission")
+
+public class MyLocationActivity extends AppCompatActivity implements LocationListener {
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    lehbas();
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3);
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
 
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        updateLocationText(location);
+            //Requisitar a permissão para o usuarios
+            //requestPermissionsResultLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            return;
+        }
+        lehbas();
+
     }
 
     @Override
@@ -34,5 +63,15 @@ public class MyLocationActivity extends Activity implements LocationListener {
     private void updateLocationText(Location location) {
         TextView textView = (TextView) findViewById(R.id.textView2);
         textView.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @SuppressLint("MissingPermission")
+    private void lehbas() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location != null) {
+            updateLocationText(location);
+        }
     }
 }
